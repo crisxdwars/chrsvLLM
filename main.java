3ae47853937b625beec924adbe1ea0b86fc46c5f
crisxdwars <crisxdwars@gmail.com>
2026-05-03 01:26:35 +0800

    updated

--- main.java
+++ main.java
@@ -1,11 +1,12 @@
-import java.util.*;
+import src.a1.A1;
 
 public class main {
 int x = 5;
 	
 	public static void main(String[] args) {
 	main ah = new main();
-		System.out.println("Hello World");
+	A1 shit = new A1();
+		System.out.println(shit);
 		System.out.println(ah.x);
 	}
 }
---
+++ src/a1/A1.java
@@ -0,0 +1,8 @@
+package src.a1;
+
+public class A1 {
+
+    public static void main(String[] args) {
+        int a = 1;
+    }
+}
