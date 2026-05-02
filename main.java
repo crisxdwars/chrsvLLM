d63b22d1a55a05bb61d0fb76b4964f0da625d7e4
crisxdwars <crisxdwars@gmail.com>
2026-05-03 01:02:00 +0800

    Created Files

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class main {
+int x = 5;
+	
+
+	public static void main(String[] args) {
+	main ah = new main();
+		System.out.println("Hello World");
+		System.out.println(ah.x);
+	}
+}
