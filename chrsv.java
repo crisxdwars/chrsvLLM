1514d786a786c3fea95464c9e67963504e7fcdf4
crisxdwars <crisxdwars@gmail.com>
2026-05-06 00:20:30 +0800

    Added Session

--- chrsv.java
+++ chrsv.java
@@ -10,8 +10,8 @@ public class chrsv {
         try {
             File memory = new File("src/temp/session.txt");
             if (memory.createNewFile()) {
-                System.out.println("New Session");
-            }
+            
+            } 
 
             String _name = "Chrsv";
             Scanner input = new Scanner(System.in);
@@ -20,6 +20,12 @@ public class chrsv {
             System.out.print("You: ");
             String user = input.nextLine();
             String filtered = user.toLowerCase();
+
+            if (memory.canRead() && memory.canWrite() && memory.exists()) {
+                System.out.println("");
+            } else {
+                System.err.println("Something Went Wrong, Its either the session can't be read, write or it doesnt exists.");
+            }
 
         } catch (IOException e) {
             System.err.println("Memory Error");
