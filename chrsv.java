73d8171e3d33407416772ae813f515ecd221bc7b
crisxdwars <crisxdwars@gmail.com>
2026-05-05 20:03:22 +0800

    updated

--- chrsv.java
+++ chrsv.java
@@ -1,26 +1,27 @@
 import src.def.greet;
 import src.brain.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class chrsv {
 
     public static void main(String[] args) {
+
+        try {
+            File memoryA1 = new File("session.txt");
+            if (memoryA1.createNewFile()) {
+
+            }
+        } catch (IOException e) {
+        }
+
         String config = "Chrsv"; 
         Scanner input = new Scanner(System.in);
         greet gt = new greet();
         model mdl = new model();
-
         System.out.print("You: ");
         String user = input.nextLine();
-        
-        if (user.contains("hello") || user.contains("hi") || user.contains("sup")) {
-            System.out.print(config + ": ");
-            System.out.println(gt.getGreet());
-        } else if (user.contains("your") & user.contains("model") || user.contains("model") || user.contains("who") & user.contains("you")) {
-            System.out.print(config + ": ");
-            System.out.println(mdl.getModel());
-        }  else {
-            System.out.println("Chrsv: Out of range");
-        }
+        String filtered = user.toLowerCase();
     }
 }
