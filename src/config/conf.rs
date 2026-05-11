3dcb7a4dde75b8a143e2a8ead6927dc373d55d92
crisxdwars <crisxdwars@gmail.com>
2026-05-11 23:56:21 +0800

    Created Model Configuration

---
+++ src/config/conf.rs
@@ -0,0 +1,25 @@
+/* 
+    Hello! This will be the configuration of the information of 
+    so Called.. 
+        "Large Language Model" (LLM).
+    I am planning to make this ai learn through: 
+    -- .txt
+    -- .pdf 
+    -- .docx
+*/ 
+
+
+struct ModelConfig {
+    model_name: String,
+    model_owner: String,
+    // Adding more configuration soon!
+}
+
+pub fn call_modelname() {
+    let model = ModelConfig {
+        model_name: String::from("Chrsv"), // Change this to anything you wanted.
+        model_owner: String::from("Christian Talurong"),  // Change this to your name, So the LLM can say that you're the owner of this model.
+    };
+
+    println!("Model Name: {}", model.model_name, model.model_owner);
+}
