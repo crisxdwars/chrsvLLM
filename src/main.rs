2bbc582433fa336dfd4b7ab34f6ca1455fe3a2f8
crisxdwars <crisxdwars@gmail.com>
2026-05-12 05:55:43 +0800

    changed variable names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::*;
+pub mod config {
+    pub mod conf;
+} 
+
 fn main() {
-    println!("Hello, world!");
+// let trigger: bool = true;
+
+    config::conf::call_modelname();
+
+/*    let greeting = vec![
+    "Hello! I am {}",
+    "Example1",
+    "Example2",
+    ];
+ */
+    println!("H!");
 }
